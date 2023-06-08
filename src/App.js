f8ae90b1240d8ac61fca09edd55d96c02e93c6e5
devanshmota <devanshmota@gmail.com>
2023-06-08 15:07:31 +0530

    Project Of Searching Git User

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const UseEffectAPI = () => {
+  const [search, setSearch] = useState("");
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(
+      "https://api.github.com/search/users?q=" + search
+    );
+    const data = await response.json();
+    setUsers(data.items);
+  };
+
+  const submit = async (event) => {
+    event.preventDefault();
+    getUsers();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container">
+        <div className="row text-center">
+          <form onSubmit={submit}>
+            <div className="row mt-5">
+              <div className="col-sm-3"></div>
+              <div className="col-sm-6">
+                <div className="input-group">
+                  <input
+                    id="search"
+                    value={search}
+                    type="text"
+                    className="form-control"
+                    placeholder="Search A Github User"
+                    onChange={(event) => setSearch(event.target.value)}
+                  />
 
-export default App;
+                  <div className="input-group-append">
+                    <button
+                      className="btn btn-primary"
+                      type="button"
+                      onClick={getUsers}
+                    >
+                      Search
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-3"></div>
+            </div>
+          </form>
+
+          {users.map((user) => {
+            return (
+              <div className="col-sm-3 mt-5">
+                <div className="card">
+                  <img
+                    src={user.avatar_url}
+                    className="card-img-top"
+                    alt="img"
+                  />
+                  <div className="card-body text-center">
+                    <h5 className="card-title">{user.login}</h5>
+                    <a href={user.html_url} className="btn btn-primary">
+                      Check Profile
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <br />
+    </>
+  );
+};
+
+export function App() {
+  return <UseEffectAPI />;
+}
